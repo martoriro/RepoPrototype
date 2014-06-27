18e041f30fef8d87ebd3d5dcae4ba56755e00dd6
Martín González <marto.gtk@gmail.com>
2014-06-27 11:06:23 -0400

    Download de archivos "funcionando", falta testeo y entender el upload.

---
+++ SolvingServer/src/java/sessionbeans/SolicitudFacadeLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sessionbeans;
+
+import entities.Solicitud;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Glass
+ */
+@Local
+public interface SolicitudFacadeLocal {
+
+    void create(Solicitud solicitud);
+
+    void edit(Solicitud solicitud);
+
+    void remove(Solicitud solicitud);
+
+    Solicitud find(Object id);
+
+    List<Solicitud> findAll();
+
+    List<Solicitud> findRange(int[] range);
+
+    int count();
+    
+}
